9ca47dadcfd45a5460e0d5d4870ea9d3464a834a
JerryUbuntu <jerryshum2636@gmail.com>
2024-06-21 11:33:00 -0400

    feat: initialize weather app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+   return (
+      <>
+         <h1 className="font-semibold text-2xl">Hello World!</h1>
+         <h2>Hello World 2!</h2>
+      </>
+   );
+}
+
+export default App;
