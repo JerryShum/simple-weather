ae7c480af308552fc1b1901be8d0e8fb59555ab6
JerryUbuntu <jerryshum2636@gmail.com>
2024-06-22 13:34:03 -0400

    feat: implemented location searching and conditional loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,88 @@ import { useState } from "react";
 import { useEffect } from "react";
 import WeatherList from "./components/WeatherList";
 import WeatherCard from "./components/WeatherCard";
+import Button from "./components/Button";
 
 function App() {
-   const [weatherData, setWeatherData] = useState({});
-   const forecast = weatherData["days"];
-   console.log(forecast);
+   const [latLng, setLatLng] = useState("");
+   const [weatherData, setWeatherData] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const forecast = weatherData ? weatherData["days"] : "";
 
-   useEffect(function () {
-      async function apiCall() {
-         try {
-            const response = await fetch(
-               "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toronto?unitGroup=metric&iconSet=icons2&key=HSCGAMJMGPCQ2H4TL673GQABT&contentType=json"
-            );
+   useEffect(
+      function () {
+         async function apiCall() {
+            setLoading(true);
+            try {
+               if (location) {
+                  const response = await fetch(
+                     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latLng}?unitGroup=metric&iconSet=icons2&key=HSCGAMJMGPCQ2H4TL673GQABT&contentType=json`
+                  );
 
-            const data = await response.json();
-            setWeatherData(data);
-            console.log(data);
-         } catch (error) {
-            console.log("Error! : " + error);
+                  const data = await response.json();
+                  setWeatherData(data);
+                  console.log(data);
+               } else {
+                  let error = new Error("Location does not exist.");
+                  throw error;
+               }
+            } catch (error) {
+               console.log("Error! : " + error);
+            }
+            setLoading(false);
          }
+
+         apiCall();
+      },
+      [latLng]
+   );
+
+   function handleClick() {
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+         console.log("Geolocation not supported");
+         return;
       }
 
-      apiCall();
-   }, []);
+      function success(position) {
+         const latitude = position.coords.latitude;
+         const longitude = position.coords.longitude;
+         setLatLng(`${latitude}, ${longitude}`);
+      }
+
+      function error() {
+         console.log("Unable to retrieve your location");
+      }
+   }
 
    return (
       <div className="h-screen flex flex-col justify-center items-center">
          <h1 className="mb-5 font-bold text-4xl">Simple Weather</h1>
-         <h2 className="mb-5 font-medium text-2xl">Location - Toronto</h2>
 
-         <WeatherList>
-            {forecast?.slice(1, 8).map((day, index) => (
-               <WeatherCard
-                  icon={day["icon"]}
-                  date={day["datetime"]}
-                  temperature={day["feelslike"]}
-                  description={day["description"]}
-                  key={index}
-               />
-            ))}
-         </WeatherList>
+         {loading ? (
+            <p>Loading...</p>
+         ) : weatherData ? (
+            <>
+               <h2 className="mb-5 font-medium text-2xl">
+                  Location - {weatherData["timezone"]?.split("/").slice(1, 2)}
+               </h2>
+
+               <WeatherList>
+                  {forecast?.slice(1, 8).map((day, index) => (
+                     <WeatherCard
+                        icon={day["icon"]}
+                        date={day["datetime"]}
+                        temperature={day["feelslike"]}
+                        description={day["description"]}
+                        key={index}
+                     />
+                  ))}
+               </WeatherList>
+            </>
+         ) : (
+            <Button handleClick={handleClick}>Get Location</Button>
+         )}
       </div>
    );
 }
